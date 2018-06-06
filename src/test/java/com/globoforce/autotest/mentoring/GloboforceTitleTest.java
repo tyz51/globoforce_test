a8d26bbb3f08ebbcfff468f496df8e1d46ed90fc
Iryna_Aleinik <iryna_aleinik@epam.com>
2018-06-06 23:43:45 +0300

    Add test

---
+++ src/test/java/com/globoforce/autotest/mentoring/GloboforceTitleTest.java
@@ -0,0 +1,26 @@
+package com.globoforce.autotest.mentoring;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class GloboforceTitleTest {
+
+    @Test
+    public void test() throws MalformedURLException {
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setBrowserName("chrome");
+        cap.setPlatform(Platform.WINDOWS);
+        URL url = new URL("http://10.9.126.5:4444/wd/hub");
+        WebDriver driver = new RemoteWebDriver(url, cap);
+        driver.get("https://www.globoforce.com/");
+        Assert.assertEquals(driver.getTitle(), "Globoforce | Elevate Workplace Culture and Performance | Globoforce");
+        driver.quit();
+    }
+}
