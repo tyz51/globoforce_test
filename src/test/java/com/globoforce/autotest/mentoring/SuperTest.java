ce862c7eb129b63da5c903f2e9a9a3453d5ea4e6
Pavel Paltarzhytski <Pavel_Paltarzhytski@epam.com>
2020-03-17 17:51:48 +0300

    Added SuperTest

---
+++ src/test/java/com/globoforce/autotest/mentoring/SuperTest.java
@@ -0,0 +1,12 @@
+package com.globoforce.autotest.mentoring;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SuperTest {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+}
